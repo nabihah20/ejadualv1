fbac4a8568dcecae75c7a08470e2409c7ea0e8d6
nabihah20 <nabihah.student@gmail.com>
2019-03-04 17:09:13 +0800

    add login form sources

---
+++ index.php
@@ -0,0 +1,214 @@
+    <!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="codepixer">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>eJadual - Sistem Penjadualan Mesyuarat </title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/nice-select.css">					
+			<link rel="stylesheet" href="css/animate.min.css">
+			<link rel="stylesheet" href="css/owl.carousel.css">
+			<link rel="stylesheet" href="css/main.css">
+
+			<!-- Loginform -->
+			<link rel="stylesheet" href="loginform/assets/css/form-elements.css">
+
+		</head>
+		<body>
+
+			  <header id="header" id="home">
+			    <div class="container">
+			    	<div class="row align-items-center justify-content-between d-flex">
+				      <div id="logo">
+				        <a href="index.php"><img src="img/logoeJ.png" style="width: 131px; height:20px" alt="" title="eJadual" /></a>
+				      </div>
+				      <nav id="nav-menu-container">
+				        <ul class="nav-menu">
+				          <li class="menu-active"><a href="#home"><span class="glyphicon glyphicon-home">Utama</span></a></li>
+				          <li><a href="#project">Cara Penggunaan</a></li>
+				          <li><a href="#senarai">Senarai</a></li>
+				          <li><a href="login.php">Log Masuk</a></li>
+				          <li class="menu-has-children"><a href="#">Jadual</a>
+				            <ul>
+				              <li><a href="jadualsu.html">Setiausaha Bandaran</a></li>
+				              <li><a href="jadualmesy.html">Mesyuarat</a></li>
+				            </ul>
+									</li>
+				        </ul>
+				      </nav><!-- #nav-menu-container -->		    		
+			    	</div>
+			    </div>
+			  </header><!-- #header -->
+
+			<!-- start banner Area -->
+			<section class="banner-area" id="home">	
+				<div class="container">
+					<div class="row fullscreen d-flex align-items-center justify-content-center">
+						<div class="banner-content col-lg-7">
+							<h1>
+								eJadual			
+							</h1>
+							<h2>Sistem Penjadualan Mesyuarat</h2>
+							<h4>
+								Majlis Bandaraya Ipoh	
+							</h4>
+							<br/>
+							<button type="submit" class="btn">Lihat Jadual Mesyuarat</button>
+						</div>				
+					</div>
+
+				</div>
+				
+			</section>
+			<!-- End banner Area -->	
+
+			<!-- Start protfolio Area -->
+			<section class="protfolio-area section-gap" id="project">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content pb-60 col-lg-10">
+							<div class="title text-center">
+								<h1 class="mb-10">Cara Penggunaan</h1>
+								<p>Mudah, cepat dan benar</p>
+							</div>
+						</div>
+					</div>						
+					<div class="row">
+						<div class="col-lg-3 single-portfolio">
+                            <h4>Langkah 1</h4>
+                            <hr>
+                            <h5>Cari tarikh dan masa untuk melihat mesyuarat yang ada</h5>
+                            <img class="image img-fluid" src="img/langkah1.png" alt="">
+							<a href="img/langkah1.png" class="img-pop-up">	
+								<div class="middle">
+									<div class="text"><span class="lnr lnr-frame-expand"></span></div>
+								</div>
+                            </a>
+                        </div>
+						<div class="col-lg-3 single-portfolio">
+                            <h4>Langkah 2</h4>
+                            <hr>
+                            <h5>Klik nama mesyuarat untuk melihat maklumat lanjut</h5>
+                            <img class="image img-fluid" src="img/langkah1.png" alt="">
+							<a href="img/langkah1.png" class="img-pop-up">	
+								<div class="middle">
+									<div class="text"><span class="lnr lnr-frame-expand"></span></div>
+								</div>
+                            </a>
+                        </div>
+						<div class="col-lg-3 single-portfolio">
+                            <h4>Langkah 3</h4>
+                            <hr>
+                            <h5>Klik '+' untuk tambah mesyuarat baru </h5>
+							<img class="image img-fluid" src="img/langkah1.png" alt="">
+							<a href="img/langkah1.png" class="img-pop-up">	
+								<div class="middle">
+									<div class="text"><span class="lnr lnr-frame-expand"></span></div>
+								</div>
+                            </a>
+                        </div>
+						<div class="col-lg-3 single-portfolio">
+                            <h4>Langkah 4</h4>
+                            <hr>
+                            <h5>Isi maklumat mesyuarat yang akan dijalankan</h5>
+							<img class="image img-fluid" src="img/langkah1.png" alt="">
+							<a href="img/langkah1.png" class="img-pop-up">	
+								<div class="middle">
+									<div class="text"><span class="lnr lnr-frame-expand"></span></div>
+								</div>
+                            </a>
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End protfolio Area -->			
+
+			<!-- Start home-video Area -->
+			<section class="protfolio-area section-gap" id="senarai">
+					<div class="container">
+						<div class="row d-flex justify-content-center">
+							<div class="menu-content pb-60 col-lg-10">
+								<div class="title text-center">
+									<h1 class="mb-10">Senarai Lokasi Mesyuarat</h1>
+									<p>Bilik yang dilengkapi sistem siar raya, projektor dan berhawa dingin</p>
+								</div>
+							</div>
+						</div>						
+						<div class="row">
+								<table class="table table-hover table-dark">
+									<thead>
+										<tr>
+										<th scope="col">#</th>
+										<th scope="col">ID</th>
+										<th scope="col">Nama</th>
+										</tr>
+                                    </thead>
+									<tbody>
+                                    <?php
+                                        require_once('connection.php');
+                                        $result = $conn->prepare("SELECT * FROM bilik");
+                                        $result->execute();
+                                        for($i=0; $row = $result->fetch(); $i++){
+                                    ?>
+										<tr>
+                                        <td><?php echo $row['id'];?></td>
+										<td><?php echo $row['bilik_id']; ?></td>
+										<td><?php echo $row['nama']; ?></td>
+                                        </tr>
+                                    <?php } ?>
+									</tbody>
+								</table>			
+						</div>
+					</div>	
+				</section>
+			<!-- End home-aboutus Area -->
+		
+			<!-- End we-offer Area -->
+			<!-- start footer Area -->		
+			<footer class="footer-area" >
+				<div class="container" style="height: 100px;">
+						<p class="col-lg-8 col-sm-12 footer-text m-0 text-white">
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Hakcipta Terpelihara &copy;<script>document.write(new Date().getFullYear());</script> <a href="http://www.mbi.gov.my/">Majlis Bandaraya Ipoh</a> | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						</p>
+				</div>
+			</footer>
+			<!-- End footer Area -->		
+
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>			
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+  			<script src="js/easing.min.js"></script>			
+			<script src="js/hoverIntent.js"></script>
+			<script src="js/superfish.min.js"></script>	
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>	
+			<script src="js/owl.carousel.min.js"></script>			
+			<script src="js/jquery.sticky.js"></script>
+			<script src="js/jquery.nice-select.min.js"></script>			
+			<script src="js/parallax.min.js"></script>		
+			<script src="js/mail-script.js"></script>	
+			<script src="js/main.js"></script>	
+		</body>
+	</html>
