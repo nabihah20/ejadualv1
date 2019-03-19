6ffc952713f5a3c52b1b3f68508c237b6a58783d
nabihah20 <nabihah.student@gmail.com>
2019-03-19 10:45:48 +0800

    basic profil

--- profil.php
+++ profil.php
@@ -20,12 +20,12 @@
 <?php include "headerhome.php"; ?>
 
 <!-- Section: about -->
-<section id="bilik" class="home-section color-dark bg-white">
+<section id="profil" class="home-section color-dark bg-white">
     <div class="container marginbot-50">
       <div class="row">
         <div class="col-lg-8 col-lg-offset-2">
           <div class="animatedParent">
-            <div class="section-heading text-center">
+            <div class="section-heading text-center animated bounceInDown">
               <h2 class="h-bold">Profil Saya</h2>
               <div class="divider-header"></div>
             </div>
@@ -34,33 +34,114 @@
       </div>
     </div>
 
-    <div class="container">
-      <div class="row">
+
+  <!--Main container. Everything must be contained within a top-level container.-->
+  <div class="container">
+
+    <!--First row (only row)-->
+    <div class="row">
+
+      <!-- First column (smaller of the two). Will appear on the left on desktop and on the top on mobile. -->
+      <div class="col-md-4 col-sm-12 col-xs-12">
+
+          <!-- Div to center the header image/name/social buttons -->
+          <div class="text-center">
+
+              <!-- Placeholder image using Placeholder.com -->
+              <img src="http://via.placeholder.com/300x250" class="img-rounded"/>
+
+              <!-- Header text (Person's name) -->
+              <h4><?php echo $userRow['user_name']; ?></h4>
+
+              <!-- Social buttons using anchor elements and btn-primary class to style -->
+              <table class="table table-hover table-dark">
+                <tr>
+                  <th>ID Staf:</th>
+                  <td><?php echo $userRow['user_id'];?></td>
+                </tr>
+                <tr>
+                  <th>Nama:</th>
+                  <td><?php echo $userRow['user_name']; ?></td>
+                </tr>
+                <tr>
+                  <th>Emel:</th>
+                  <td><?php echo $userRow['user_email']; ?></td>
+                </tr>
+            </table>                  
+          </div>
+
+      </div> <!-- End Col 1 -->
+
+      <!-- Second column - for small and extra-small screens, will use whatever # cols is available -->
+      <div class="col-md-8 col-sm-* col-xs-*">
+
+        <!-- "Lead" text at top of column. -->
+        <p class="lead">Senarai Mesyuarat yang didaftarkan</p>
+
+        <!-- Horizontal rule to add some spacing between the "lead" and body text -->
+        <hr />
+
         <table class="table table-hover table-dark">
-          <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">ID</th>
-            <th scope="col">Nama</th>
-            </tr>
-          </thead>
-          <tbody>
+          <?php
+            include('connection.php');
+              $sql = "SELECT * FROM mesy 
+              WHERE user_id='$id'";
+              $statement = $conn->prepare($sql);
+              $statement->execute();
+              $result = $statement->fetchAll();
+              
+             if ($result && $statement->rowCount() > 0) {
+          ?>
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Nama</th>
+                  <th scope="col">Tarikh</th>
+                  <th scope="col">Masa</th>
+                  <th scope="col">Lokasi</th>
+                  <th scope="col">Status</th>
+                </tr>
+              </thead>
             <?php
-                require_once('connection.php');
-                $result = $conn->prepare("SELECT * FROM bilik");
-                $result->execute();
-                for($i=0; $row = $result->fetch(); $i++){
+              $counter = 1; 
+              foreach ($result as $row) {
+                  
+                  $title = $row['title'];
+                  $mesy_tarikh = $row['mesy_tarikh'];
+                  $start = $row['start'];
+                  $mesy_lokasi = $row['mesy_lokasi'];
+                  $mesy_status = $row['mesy_status'];
             ?>
-            <tr>
-              <td><?php echo $row['id'];?></td>
-              <td><?php echo $row['bilik_id']; ?></td>
-              <td><?php echo $row['bilik_nama']; ?></td>
-            </tr>
-            <?php } ?>
+
+            <tbody>
+              <tr>
+                <td><?php echo $counter; ?></td>
+                <td><?php echo $title; ?></td>
+                <td><?php echo $mesy_tarikh; ?></td>
+                <td><?php echo $start; ?></td>
+                <td><?php echo $mesy_lokasi; ?></td>
+                <td><?php echo $mesy_status; ?></td>
+              </tr>
+            <?php $counter++; 
+        }
+
+    } else {
+        ?> 
+        <tr>
+            <td>Tiada mesyuarat didaftarkan lagi</td>
+        </tr>
+<?php
+    }
+?>
           </tbody>
-        </table>			
-      </div>                      
-    </div>
+        </table>	
+
+      </div> <!-- End column 2 -->
+
+    </div> <!-- End row 1 -->
+
+  </div> <!-- End main container -->
+
   </section>
   <!-- /Section: about -->
 
